7d9afc5012188cbe37d7eca61535327e88f1c75f
Mike Kalagan <mike.kalagan@gmail.com>
2026-05-04 09:47:32 +0500

    fix: suppress clippy not_unsafe_ptr_arg_deref on AudioInput::start

--- rill-io/src/input.rs
+++ rill-io/src/input.rs
@@ -99,6 +99,7 @@ impl<T: Transcendental, const BUF_SIZE: usize> AudioInput<T, BUF_SIZE> {
     /// `nodes_ptr` must point to the graph's node array (obtained from
     /// `graph.into_parts().0.into_boxed_slice()`). Valid until `stop()`.
     /// `source_idx` is this node's index in the array.
+    #[allow(clippy::not_unsafe_ptr_arg_deref)]
     pub fn start(
         &mut self,
         nodes_ptr: *mut [NodeVariant<f32, BUF_SIZE>],
