9cc6ebc5075e2e54f9c3859a6ea92984986ab6cb
Mike Kalagan <mike.kalagan@gmail.com>
2026-05-08 20:07:39 +0500

    feat(rill-lofi): extract Ay38910Chip with pure AY-3-8910 logic

--- rill-lofi/src/emulators/mod.rs
+++ rill-lofi/src/emulators/mod.rs
@@ -1,7 +1,9 @@
 mod akai_s900;
 mod ay38910;
+mod ay38910_chip;
 mod nes;
 
 pub use akai_s900::AkaiS900Emulator;
 pub use ay38910::Ay38910Emulator;
+pub use ay38910_chip::Ay38910Chip;
 pub use nes::NesEmulator;
